5341d344f7c29c22afd9f095c7e31978c0654008
sagiegurari <sagiegurari@gmail.com>
2016-07-08 17:45:44 +0300

    test

--- test.sh
+++ test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-git clone git@github.com:sagiegurari/simple-oracledb.git
-cd simple-oracledb
+npm install simple-oracledb
+cd ./node_modules/simple-oracledb
 
 export NODE_ENV=development
-export TEST_ORACLE_USER=system
-export TEST_ORACLE_PASSWORD=oracle
-export TEST_ORACLE_CONNECTION_STRING=localhost/XE
 
 npm install
 npm install oracledb
+
+export TEST_ORACLE_USER=system
+export TEST_ORACLE_PASSWORD=oracle
+export TEST_ORACLE_CONNECTION_STRING=localhost/XE
 
 npm test
