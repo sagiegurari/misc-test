2bd44c72c5a3a7f3c28b964999f6e3ce90fed3b5
sagiegurari <sagiegurari@gmail.com>
2016-07-09 16:42:31 +0300

    build instructions

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ docker pull wnameless/oracle-xe-11g
 
 docker build -t sagiegurari/test:1.0 .
 
-docker stop $(docker ps -a -q)
+#docker stop $(docker ps -a -q)
 
 #-p 49160:22 
 docker run -d -p 1521:1521 -e ORACLE_ALLOW_REMOTE=true wnameless/oracle-xe-11g
