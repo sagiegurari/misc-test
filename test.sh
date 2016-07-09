e4fc75700ab0397d9476c6c2c01ee54829d4696f
sagiegurari <sagiegurari@gmail.com>
2016-07-09 17:11:19 +0300

    build instructions

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-npm install simple-oracledb
+npm --loglevel verbose warn simple-oracledb
 cd ./node_modules/simple-oracledb
 
 export NODE_ENV=development
 
-npm install
-npm install oracledb
+npm --loglevel verbose warn install
+npm --loglevel verbose warn install oracledb
 
 export TEST_ORACLE_USER=system
 export TEST_ORACLE_PASSWORD=oracle
