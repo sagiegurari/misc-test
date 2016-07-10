fc8a12cfed5c46923e7532f01e802c84220542bd
Sagie Gur-Ari <sagiegurari@gmail.com>
2016-07-10 09:30:19 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ set -ev
 
 docker pull wnameless/oracle-xe-11g
 
-docker build -t test .
-
-#docker stop $(docker ps -a -q)
-
 #-p 49160:22 
 docker run -d -p 1521:1521 -e ORACLE_ALLOW_REMOTE=true wnameless/oracle-xe-11g
 docker ps -a
+
+docker build -t test .
+
+#docker stop $(docker ps -a -q)
 
 docker run --add-host dockerhost:`/sbin/ip route|awk '/default/ { print  $3}'` --name test -t --cidfile ./test.cid test
