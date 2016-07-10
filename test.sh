8811f72c0a0bdad736193609c69c1a219d54efab
Sagie Gur-Ari <sagiegurari@gmail.com>
2016-07-10 09:05:53 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,13 @@ wget https://github.com/sagiegurari/simple-oracledb/archive/master.zip
 unzip master.zip
 cd ./simple-oracledb-master
 
+echo "OCI_LIB_DIR: $OCI_LIB_DIR"
 tree $OCI_LIB_DIR
+echo "----------------"
 
+echo "OCI_INCLUDE_DIR: $OCI_INCLUDE_DIR"
 tree $OCI_INCLUDE_DIR
+echo "----------------"
 
 npm --loglevel warn -g --production install mocha chai
 
