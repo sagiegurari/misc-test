e7bd72ef036902f17f97888bb8c6750066585a5a
Sagie Gur-Ari <sagiegurari@gmail.com>
2016-07-10 19:51:34 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -ev
 #docker stop $(docker ps -a -q)
 
 docker pull wnameless/oracle-xe-11g
-docker run -d -p 35353:1521 -e ORACLE_ALLOW_REMOTE=true wnameless/oracle-xe-11g
+docker run --name db -d -p 35353:1521 -e ORACLE_ALLOW_REMOTE=true wnameless/oracle-xe-11g
 
 #docker pull alexeiled/docker-oracle-xe-11g
 #docker run -d -p 35353:1521 alexeiled/docker-oracle-xe-11g
@@ -19,4 +19,4 @@ echo "parent host: ${PARENT_HOST}"
 
 export DB_CONNECT_STRING="(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=${PARENT_HOST})(PORT=35353))(CONNECT_DATA=(SID=xe)))"
 
-docker run --env TEST_ORACLE_CONNECTION_STRING="${DB_CONNECT_STRING}" --add-host dockerhost:${PARENT_HOST} --name test -t --cidfile ./test.cid test
+docker run --link db:db --env TEST_ORACLE_CONNECTION_STRING="${DB_CONNECT_STRING}" --add-host dockerhost:${PARENT_HOST} --name test -t --cidfile ./test.cid test
