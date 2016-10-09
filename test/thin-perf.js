e989b52c995fdce510a03326cbc8a57b5fba2400
sagiegurari <sagiegurari@gmail.com>
2016-10-09 17:54:31 +0300

    thin perf test

---
+++ test/thin-perf.js
@@ -0,0 +1,146 @@
+var asyncLib = require('async');
+var SimpleOracleDB = require('simple-oracledb');
+
+const MAX_ROWS = 5000;
+
+var exit = process.exist;
+process.exit = function () {
+    setTimeout(exit, 500);
+};
+
+function onError(error) {
+    if (error) {
+        console.error(error);
+        process.exit();
+    }
+}
+
+function runTest(state, test, callback) {
+    var start = Date.now();
+
+    test(function (error, diff) {
+        var end = Date.now();
+
+        onError(error);
+
+        var diffFull = end - start;
+
+        state.counter++;
+
+        if ((state.min === -1) || (state.min > diff)) {
+            state.min = diff;
+        }
+        if ((state.min === -1) || (state.max < diff)) {
+            state.max = diff;
+        }
+        state.total = state.total + diff;
+
+        if ((state.minFull === -1) || (state.minFull > diffFull)) {
+            state.minFull = diffFull;
+        }
+        if ((state.maxFull === -1) || (state.maxFull < diffFull)) {
+            state.maxFull = diffFull;
+        }
+        state.totalFull = state.totalFull + diffFull;
+
+        setTimeout(callback, 0);
+    });
+}
+
+function runSuite(test, loops, callback) {
+    console.log('runSuite...');
+
+    var state = {
+        counter: 0,
+        total: 0,
+        totalFull: 0,
+        min: -1,
+        max: -1,
+        minFull: -1,
+        maxFull: -1
+    };
+
+    asyncLib.until(function () {
+        return state.counter >= loops;
+    }, function (cb) {
+        runTest(state, test, cb);
+    }, function (error) {
+        onError(error);
+
+        state.average = state.total / state.counter;
+        state.averageFull = state.totalFull / state.counter;
+        state.averageDiff = (state.averageFull - state.average).toFixed(2);
+
+        callback(null, state);
+    });
+}
+
+function runSuites(tests, loops, callback) {
+    console.log('runSuites...');
+
+    var funcs = [];
+    tests.forEach((test) => {
+        funcs.push(function (cb) {
+            runSuite(test, loops, function (error, state) {
+                onError(error);
+                console.log('runSuite end...', state);
+
+                cb(error, state);
+            });
+        });
+    });
+
+    asyncLib.series(funcs, callback);
+}
+
+function getQueryTest(dbData) {
+    var connection = {
+        query: function () {
+            arguments[arguments.length - 1](dbData);
+        }
+    };
+    SimpleOracleDB.extend(connection);
+
+    return function (callback) {
+        var diff;
+        var start = Date.now();
+        connection.query('SELECT * FROM TEST_PERF1', [], {
+            maxRows: 100000
+        }, function (error, result) {
+            diff = Date.now() - start;
+
+            if (result.length !== MAX_ROWS) {
+                callback(new Error('[QueryTest] invalid result, ' + result.length));
+            } else {
+                callback(error, diff);
+            }
+        });
+    };
+}
+
+function runAll() {
+    var i;
+    var rowsData = [];
+    for (i = 0; i < MAX_ROWS; i++) {
+        rowsData.push({
+            COL1: 'PK' + i,
+            COL2: i,
+            COL3: i % 10,
+            COL4: 'STR' + (i % 10)
+        });
+    }
+
+    var queryTest = getQueryTest(rowsData);
+
+    runSuites([ queryTest], 10, function () {
+        runSuites([queryTest], 1000, function (err, states) {
+            console.log(JSON.stringify(states, undefined, 2));
+
+            states.forEach((testStates) => {
+                console.log(testStates);
+            });
+        });
+    });
+}
+
+runAll();
