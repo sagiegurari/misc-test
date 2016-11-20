f9c1def1bb93d43eff48557b8e7c42de1e180975
sagiegurari <sagiegurari@gmail.com>
2016-11-20 17:51:44 +0200

    sort repos

--- assets/app.js
+++ assets/app.js
@@ -2,5 +2,7 @@ angular.module('siteApp', ['ngMaterial'], function ($interpolateProvider) {
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
 }).controller('siteController', ['$scope', function ($scope) {
-        $scope.repositories = window.githubInfo;
+        $scope.repositories = window.githubInfo.sort(function (repo1, repo2) {
+                return repo1.stargazers_count - repo2.stargazers_count;
+        });
 }]);
