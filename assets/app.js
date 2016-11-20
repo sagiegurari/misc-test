5c3f9d7896297e21d8fdffdb38026e3a14d85db9
sagiegurari <sagiegurari@gmail.com>
2016-11-20 19:24:36 +0200

    scss support

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,26 @@
 angular.module('siteApp', ['ngMaterial'], function ($interpolateProvider) {
-        $interpolateProvider.startSymbol('[[');
-        $interpolateProvider.endSymbol(']]');
+    'use strict';
+
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
 }).controller('siteController', ['$scope', function ($scope) {
-        $scope.repositories = window.githubInfo.sort(function (repo1, repo2) {
-                var output = repo2.stargazers_count - repo1.stargazers_count;
+    'use strict';
 
-                if (!output) {
-                        output = repo2.forks_count - repo1.forks_count;
-                }
+    $scope.repositories = window.githubInfo.sort(function (repo1, repo2) {
+        var output = repo2.stargazers_count - repo1.stargazers_count;
 
-                return output;
-        });
+        if (!output) {
+            output = repo2.forks_count - repo1.forks_count;
+        }
 
-        $scope.getLanguageColor = function (repository) {
-                if (repository && repository.language) {
-                        return 'github-color-' + repository.language.toLowerCase();
-                }
+        return output;
+    });
 
-                return '';
-        };
+    $scope.getLanguageColor = function (repository) {
+        if (repository && repository.language) {
+            return 'github-color-' + repository.language.toLowerCase();
+        }
+
+        return '';
+    };
 }]);
