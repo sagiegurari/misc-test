7a48694f21a23b735d26b8aa3b72d6daf1636fcf
sagiegurari <sagiegurari@gmail.com>
2016-11-26 19:38:17 +0200

    release tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,26 @@
 module.exports = function (grunt) {
+    var url = require('./package.json').repository.url;
+    var prefix = 'github.com/';
+    var repo = url.substring(url.indexOf(prefix) + prefix.length, url.lastIndexOf('.git'));
+    console.log(repo);
+
     grunt.initConfig({
         release: {
             options: {
-                bump: false, //default: true
-                changelog: false, //default: false
-                changelogText: '<%= version %>\n', //default: '### <%= version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n'
-                file: 'package.json', //default: package.json
-                add: false, //default: true
-                commit: false, //default: true
-                tag: false, //default: true
-                push: false, //default: true
-                pushTags: false, //default: true
-                npm: false, //default: true
-                npmtag: false, //default: no tag
-                indentation: '\t', //default: '  ' (two spaces)
-                folder: 'folder/to/publish/to/npm', //default project root
-                tagName: '<%= version %>', //default: '<%= version %>'
-                commitMessage: 'check out my release <%= version %>', //default: 'release <%= version %>'
-                tagMessage: '<%= version %>', //default: 'Version <%= version %>',
-                beforeBump: [], // optional grunt tasks to run before file versions are bumped
-                afterBump: [], // optional grunt tasks to run after file versions are bumped
-                beforeRelease: [], // optional grunt tasks to run after release version is bumped up but before release is packaged
-                afterRelease: [], // optional grunt tasks to run after release is packaged
-                updateVars: [], // optional grunt config objects to update (this will update/set the version property on the object specified)
+                bump: false,
+                changelog: false,
+                file: 'package.json',
+                add: false,
+                commit: false,
+                tag: true,
+                push: false,
+                pushTags: true,
+                npm: false,
+                npmtag: false,
+                tagName: '<%= version %>',
+                tagMessage: '<%= version %>',
                 github: {
-                    repo: 'sagiegurari/misc-test', //put your user/repo here
+                    repo: repo,
                     accessTokenVar: 'GTOKEN'
                 }
             }
