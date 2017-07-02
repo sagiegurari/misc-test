d0772913540780a951563c1351e72a68b53031eb
sagie gur ari <sagiegurari@gmail.com>
2017-07-02 16:05:33 +0000

    test

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 
 use std::path::Path;
 
-fn test(base_path: &str, file_name: &str) {
-    let file_path = Path::new(base_path).join(file_name);
+fn main() {
+    let file_path = Path::new(".").join("Cargo.toml");
 
     if file_path.exists() {
         match file_path.canonicalize() {
-            Ok(value) => println!("file found: {:?}", value),
-            Err(error) => panic!("Unable to get file path for base path: {} file name: {} error: {}", base_path, file_name, error),
+            Ok(value) => {
+                println!("file found: {:?}", value);
+                
+                let parent_path = file_path.parent().unwrap().to_str().unwrap();
+                
+                let parent_file = Path::new(parent_path).join("../test.txt");
+                
+                if parent_file.exists() {
+                    match parent_file.canonicalize() {
+                        Ok(value) => println!("parent file found: {:?}", value),
+                        Err(error) => panic!("Unable to get file path for base path: {} file name: test.txt error: {}", parent_path, error),
+                    };
+                } else {
+                    println!("parent file not found!!!");
+                }
+            }
+            Err(error) => panic!("Unable to get file path for base path: . file name: cargo toml error: {}", error),
         };
     } else {
         println!("file not found!!!");
     }
-}
-
-fn main() {
-    test(".", "Cargo.toml");
-    test("..", "test.txt");
 }
