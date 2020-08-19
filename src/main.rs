89b59265646b4ae0710a6a7bdba2060c18aa3512
sagie gur ari <sagiegurari@gmail.com>
2020-08-19 19:05:15 +0000

    initial

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#[derive(Debug, Clone, PartialEq)]
+enum SomeType {
+    A,
+    B,
+    C,
+}
+
+fn main() {
+    let mut some_type = SomeType::A;
+
+    println!("current: {:#?}", &some_type);
+
+    some_type = match some_type {
+        SomeType::A => SomeType::B,
+        _ -> SomeType::C,
+    };
+
+    println!("updated to: {:#?}", &some_type);
+}
